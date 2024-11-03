eb51589c3c5314104dc2667384af3876ee9d50f2
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2024-11-02 23:48:34 -0400

    added jwt authentication and login endpoint

--- server/app.py
+++ server/app.py
@@ -3,14 +3,19 @@ from flask import Blueprint, Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager
+from dotenv import dotenv_values
 
+dotenv = dotenv_values(".env")
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["JWT_SECRET_KEY"] = dotenv["JWT_SECRET_KEY"]
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
 
 api = Blueprint('api', __name__, url_prefix='/api')
 from auth import auth_bp
--- server/auth.py
+++ server/auth.py
@@ -2,6 +2,7 @@ from app import db, ma
 from datetime import datetime, timezone
 from flask import Blueprint, jsonify, request
 from argon2 import PasswordHasher
+from flask_jwt_extended import create_access_token
 
 
 class Users(db.Model):
@@ -11,6 +12,7 @@ class Users(db.Model):
     created_at = db.Column(
         db.DateTime, nullable=False, default=datetime.now(timezone.utc)
     )
+
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
@@ -26,6 +28,7 @@ auth_bp = Blueprint("auth", __name__, url_prefix=None)
 def register():
     user_schema = UserSchema()
     email = request.json["email"]
+
     if Users.query.filter_by(email=email).first():
         return jsonify({"message": "Email already in use"}), 409
     password = request.json["password"]
@@ -33,5 +36,17 @@ def register():
     user = Users(email=email, password_hash=hashed_password)
     db.session.add(user)
     db.session.commit()
-    return jsonify(user_schema.dump(user))
+    return jsonify(dict(message="User successfully created")), 201
 
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    user_schema = UserSchema()
+    email = request.json["email"]
+    password = request.json["password"]
+    if not (user := Users.query.filter_by(email=email).first()):
+        return jsonify({"message": "Invalid email or password"}), 401
+    if not PasswordHasher().verify(user.password_hash, password):
+        return jsonify({"message": "Invalid email or password "}), 401
+    token = create_access_token(identity=user_schema.dump(user))
+    return jsonify(user_schema.dump(user)), 200, {"Authorization": f"Bearer {token}"}
