a9bfd372d6fbd6e6da25e367d383108802c85a61
LuisDelgado <luisgabrielda@gmail.com>
2024-11-27 12:27:54 -0500

    Homepage design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,51 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import React from 'react';
+import { useRoutes, Link } from 'react-router-dom';
+import Login from './pages/Login';
+import CreateUser from './pages/CreateUser';
 
 const App = () => {
-  return (
-    <div>
-      <h1>Market Swipe</h1>
-      <h2>Display Product</h2>
-    </div>
-  )
-}
+  // Sets up routes
+  let element = useRoutes([
+    {
+      path: "/login",
+      element: (
+        <div className="fullPage">
+          <Login />
+        </div>
+      ),
+    },
+    {
+      path: "/new",
+      element: (
+        <div className="fullPage">
+          <CreateUser />
+        </div>
+      ),
+    },
+    {
+      path: "/",
+      element: (
+        <div className="App">
+          <div className="header">
+            <h1 className='mainHeader'>WELCOME TO <span>MARKET SWIPE</span></h1>
+            <h2 className="slogan">Swipe Your Way to Smarter Shopping</h2>
+          </div>
+          <div className="description">
+            <p>
+              Market Swipe is the ultimate platform for savvy shoppers. Discover, compare, and shop for the best products with ease. 
+              Whether you're looking for daily essentials or unique finds, Market Swipe helps you make informed decisions with a swipe.
+            </p>
+          </div>
+          <Link to="/login">
+            <button className="centerButton">Start Shopping Now!</button>
+          </Link>
+        </div>
+      ),
+    },
+  ]);
 
-export default App
+  return element;
+};
+
+export default App;
