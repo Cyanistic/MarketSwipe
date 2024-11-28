2fe5a721adc1bdcff6543ab6f687dfa7d6c708dc
LuisDelgado <luisgabrielda@gmail.com>
2024-11-27 22:22:34 -0500

    Login design

--- client/src/pages/CreateUser.jsx
+++ client/src/pages/CreateUser.jsx
@@ -1,13 +1,16 @@
 import './CreateUser.css';
 import React, { useState } from 'react';
-
+import NewAuthForm from '../components/NewAuthForm';
 
 const CreateUser = () => {
+
   return (
-    <div>
-
+    <div className='login-page'>
+      <h2>Create Account</h2>
+      <NewAuthForm />
     </div>
-  )
-}
+  );
+};
 
-export default CreateUser
+export default CreateUser;
+
