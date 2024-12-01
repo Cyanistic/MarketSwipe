07937399cf7cd155caec9da0e9441d88bbbcf47e
Diego <dappwow@gmail.com>
2024-12-01 15:50:33 -0500

    Added LoginPage.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,13 @@
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
-import LoginPage from './LoginPage.jsx';
+import LoginPage from './LoginPage';
 
 const App = () => {
-  const navigate = useNavigate();
-
-  const handleLoginClick = () => {
-    console.log('Log in button pressed!');
-    navigate('/login'); // Navigate to the login page
-  };
-
   return (
     <div>
-      <Routes>
-        {/* Define routes */}
-        <Route
-          path="/"
-          element={
-            <div style={{ textAlign: 'center', marginTop: '50px' }}>
-              <h1>Market Swipe</h1>
-              <button
-                onClick={handleLoginClick}
-                style={{
-                  padding: '10px 20px',
-                  backgroundColor: '#007BFF',
-                  color: 'white',
-                  border: 'none',
-                  borderRadius: '5px',
-                  cursor: 'pointer',
-                  marginTop: '20px',
-                }}
-              >
-                Log In 
-              </button>
-            </div>
-          }
-        />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
+      <LoginPage />
     </div>
   );
 };
+
 
 export default App;
