b13fd357facce2e3d0caa492f08e56391ce4d91d
LuisDelgado <luisgabrielda@gmail.com>
2024-12-03 21:40:47 -0500

    swiping logic fix

--- client/src/components/SwipeProducts.jsx
+++ client/src/components/SwipeProducts.jsx
@@ -17,7 +17,7 @@ const SwipeProducts = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get(`${BASE_URL}/api/products`);
+        const response = await axios.get(`/products`);
         setProducts(response.data);
       } catch (error) {
         console.error("Error fetching products:", error);
